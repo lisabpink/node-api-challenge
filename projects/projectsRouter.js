7008904535d45f1784050b8a8a4b825c1aabb095
LISA_COOK <lisabpink@gmail.com>
2020-02-14 12:39:48 -0500

    LDC Update a project

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -4,43 +4,99 @@ const projectDb = require("../data/helpers/projectModel.js");
 
 const router = express.Router();
 
+//!TEST
 // router.get("/", (req, res) => {
 //     res.send("projectsRouter working!")
 // });
-//!Get all projects 
+
+//!Get all projects
 router.get("/", (req, res) => {
-    projectDb.get()
+    
+  projectDb
+    .get()
     .then(projects => {
-        res.status(200).json(projects);
+      res.status(200).json(projects);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: "The projects information could not be found"
+      console.log(err);
+      res.status(500).json({
+        error: "The projects information could not be found"
+      });
+    });
+});
+
+//! Get project by id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  projectDb
+    .get(id)
+    .then(specificP => {
+      if (id) {
+        res.status(200).json(specificP);
+      } else {
+        res.status(404).json({
+          error: "No project with that ID"
         });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: "The project information could not be found"
+      });
     });
 });
 
 //!Post new project
 router.post("/", (req, res) => {
-    const newProject = req.body;
+  const newProject = req.body;
 
-    projectDb.insert(newProject)
+  projectDb
+    .insert(newProject)
     .then(project => {
-        if (newProject.name || newProject.description) {
-            res.status(201).json(project);
-        } else {
-            res.status(400).json({
-                errorMessage: "Please provide name and description for new a project"
-            })
-        }
+      if (newProject.name || newProject.description) {
+        res.status(201).json(project);
+      } else {
+        res.status(400).json({
+          errorMessage: "Please provide name and description for new a project"
+        });
+      }
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: "There was an error while saving the project"
-        });
+      console.log(err);
+      res.status(500).json({
+        error: "There was an error while saving the project"
+      });
     });
 });
 
-module.exports = router; 
+//! Put/update a project
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  projectDb
+    .update(id, body)
+    .then(updatedP => {
+      if (!id) {
+        res.status(404).json({
+          message: "The project with the specific ID does not exist"
+        });
+      } else if (!updatedP.name || !updatedP.description) {
+        res.status(400).json({
+          message: "Please provide name and description for updated project"
+        });
+      } else {
+        res.status(200).json({ updatedP });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: "The project information could not be updated"
+      });
+    });
+});
+
+module.exports = router;
