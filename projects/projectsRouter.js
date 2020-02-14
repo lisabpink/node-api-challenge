cf7adeeb8052381cc51eb18ea990239882fc0613
LISA_COOK <lisabpink@gmail.com>
2020-02-14 12:45:59 -0500

    LDC Get project action by id, Delete project

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const projectDb = require("../data/helpers/projectModel.js");
 
+const actionsDb = require("../data/helpers/actionModel.js");
+
 const router = express.Router();
 
 //!TEST
@@ -11,7 +13,7 @@ const router = express.Router();
 
 //!Get all projects
 router.get("/", (req, res) => {
-    
+
   projectDb
     .get()
     .then(projects => {
@@ -47,6 +49,28 @@ router.get("/:id", (req, res) => {
       });
     });
 });
+
+//! get a projects actions by ID
+router.get("/:id/actions", (req, res) => {
+    const project_id = req.params.id
+
+    projectDb.getProjectActions(project_id)
+    .then(actions => {
+        if (project_id) {
+            res.status(200).json(actions);
+        } else {
+            res.status(404).json({
+                message: "The project with the specific ID does not exist"
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: "The actions information could not be found"
+        })
+    })
+})
 
 //!Post new project
 router.post("/", (req, res) => {
@@ -98,5 +122,28 @@ router.put("/:id", (req, res) => {
       });
     });
 });
+
+//! Ddelete a project
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+  
+    projectDb
+      .remove(id)
+      .then(deletedP => {
+        if (!id) {
+          res.status(404).json({
+            message: "The project with the specific ID does not exist"
+          });
+        } else {
+          res.status(200).json({ deletedP });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: "The project could not be removed"
+        });
+      });
+  });
 
 module.exports = router;
