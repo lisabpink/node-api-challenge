c3627f0acb3846444bc4adf92f5d7de114713355
LISA_COOK <lisabpink@gmail.com>
2020-02-14 12:30:41 -0500

    LDC Post new project

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-const projectDb = require("../data/helpers/projectModel.js")
+const projectDb = require("../data/helpers/projectModel.js");
 
 const router = express.Router();
 
 // router.get("/", (req, res) => {
 //     res.send("projectsRouter working!")
 // });
-
+//!Get all projects 
 router.get("/", (req, res) => {
     projectDb.get()
     .then(projects => {
@@ -20,4 +20,27 @@ router.get("/", (req, res) => {
         });
     });
 });
+
+//!Post new project
+router.post("/", (req, res) => {
+    const newProject = req.body;
+
+    projectDb.insert(newProject)
+    .then(project => {
+        if (newProject.name || newProject.description) {
+            res.status(201).json(project);
+        } else {
+            res.status(400).json({
+                errorMessage: "Please provide name and description for new a project"
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: "There was an error while saving the project"
+        });
+    });
+});
+
 module.exports = router; 
